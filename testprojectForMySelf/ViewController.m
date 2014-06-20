9752ce2b2e4bf1d10e411cae941dc034ae1c9fe6
川端伸彦 <kawabatanobuhiko@kawabatanobuhiko-no-MacBook-Pro.local>
2014-06-20 09:52:58 +0900

    hello

--- testprojectForMySelf/ViewController.m
+++ testprojectForMySelf/ViewController.m
@@ -5,15 +5,23 @@
 //  Created by 川端伸彦 on 2014/06/16.
 //  Copyright (c) 2014年 mikke. All rights reserved.
 //
-
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *touchNumber;
+@property int counter;
 @end
 
 @implementation ViewController
-
+//画面をタッチしたとき
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    
+    _touchNumber.text = [NSString stringWithFormat:@"%d", _counter];
+    _counter = [[_touchNumber text] intValue];
+    _counter++;
+    
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
